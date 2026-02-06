cfb2bdd7b00daa9962ab9677c89a3b1c7f0113b0
Duaa2fatima <zaididuaa16@gmail.com>
2026-02-07 00:43:37 +0200

    first commit

--- js/script.js
+++ js/script.js
@@ -10,28 +10,28 @@ const container = document.querySelector(".cards-container");
 category_filter_btn[0].classList.add("active_category");
 
 category_filter_btn.forEach((btn) => {
-    btn.addEventListener("click", () => {
+  btn.addEventListener("click", () => {
 
-        category_filter_btn.forEach(b => b.classList.remove("active_category"));
-        btn.classList.add("active_category");
+    category_filter_btn.forEach(b => b.classList.remove("active_category"));
+    btn.classList.add("active_category");
 
-        const selectedCategory = btn.innerText.trim().toLowerCase();
+    const selectedCategory = btn.innerText.trim().toLowerCase();
 
-        fetch("../../api/products.json")
-            .then(res => res.json())
-            .then(data => {
+    fetch("../../api/products.json")
+      .then(res => res.json())
+      .then(data => {
 
-                let html = "";
+        let html = "";
 
-                const filteredData =
-                    selectedCategory === "all"
-                        ? data
-                        : data.filter(item =>
-                            item.category.toLowerCase() === selectedCategory
-                        );
+        const filteredData =
+          selectedCategory === "all"
+            ? data
+            : data.filter(item =>
+              item.category.toLowerCase() === selectedCategory
+            );
 
-                filteredData.forEach(element => {
-                    html += `
+        filteredData.forEach(element => {
+          html += `
           <a href="./product-detail.html?id=${element.id}">
             <div class="card">
               <div class="card-banner">
@@ -50,19 +50,65 @@ category_filter_btn.forEach((btn) => {
             </div>
             </a>
           `;
-                });
+        });
 
-                container.innerHTML = html;
-            });
-    });
+        container.innerHTML = html;
+      });
+  });
 });
 
 toggle.addEventListener("click", () => {
-    mobile_nav.classList.toggle("hide");
+  mobile_nav.classList.toggle("hide");
 });
 
 mobileDropdown.addEventListener('click', () => {
-    mobileDropdownItems.classList.toggle('hide');
+  mobileDropdownItems.classList.toggle('hide');
 });
 
 
+// For All
+fetch("./data/products.json")
+  .then((res) => res.json())
+  .then((data) => {
+    let bottle = "";
+    data.forEach((element) => {
+      bottle += `
+            <a href="./product-detail.html?id=${element.id}">
+            <div class="card">
+                <div class="card-banner">
+                    <img src="${element.img}" alt="">
+                </div>
+                <div class="card-text">
+                <div>
+                    <h3>${element.name}</h3>
+                    <p>${element.capacity}</p>
+                </div>
+                <div>
+                    <p>${element.category}</p>
+                    <p>RS/- ${element.price}</p>
+                </div>
+                </div>
+            </div>
+            </a>
+        `;
+
+      document.getElementsByClassName("cards-container")[0].innerHTML = bottle;
+    });
+  });
+
+
+
+fetch("./data/gallery.json")
+  .then((res) => res.json())
+  .then((data) => {
+    let image = "";
+    data.forEach((element) => {
+      image += `
+            <div class="gallery-img">
+                <img src="${element.item}" alt="">
+            </div>
+        `;
+
+      document.getElementsByClassName("gallery-container")[0].innerHTML = image;
+    });
+  });
